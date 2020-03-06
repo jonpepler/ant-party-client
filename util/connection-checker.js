fc7977c68d1297fb24218eee2a692ec7264a6f2b
Jon Pepler <jonathan.pepler@gmail.com>
2020-03-06 00:56:50 +0000

    fix: 🐛 Exit program when connection attempt fails

--- util/connection-checker.js
+++ util/connection-checker.js
@@ -1,20 +1,20 @@
 const ora = require('ora')
 const axios = require('axios')
 
-module.exports = (host) => {
+const check = async (host) => {
   const spinner = ora(`Testing connection to ${host}...`).start()
 
   const url = `${host}/api/v1/test`
-  axios.get(url)
-    .then(response => {
-      spinner.succeed(`Connected! ${response.data}`)
-    })
-    .catch(error => {
-      spinner.fail(`Failed to connect to ${url}:
-  ${error}
-      `)
-    })
-    .finally(() => {
-      spinner.stop()
-    })
+  try {
+    const { data } = await axios.get(url)
+    spinner.succeed(`Connected! ${data}`)
+  } catch (error) {
+    spinner.fail(`Failed to connect to ${url}:
+${error}
+    `)
+    process.exit()
+  } finally {
+    spinner.stop()
+  }
 }
+module.exports = check
